edb979d5e60dd47075b218afeada4d1eb0139010
Drew <drewmaatman2gmail.com>
2020-06-09 21:36:38 -0500

    Added MCU firmware project

---
+++ sfw/U601_firmware/PIC32MZ_ETH_Demo.X/main.c
@@ -0,0 +1,27 @@
+/*
+ * File:   main.c
+ * Author: Drew Maatman
+ *
+ * Created on March 8th, 2020, 12:16 PM
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Core Drivers
+#include "configuration.h"
+
+
+void main(void) {
+    
+    
+    // Main loop, do this stuff forever and ever and never get tired of it
+    while (1) {
+        
+        
+    }
+    
+}
